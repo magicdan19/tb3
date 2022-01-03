e6d9b85c649a4310dfec04c4f48dd3dce90a89d3
magicdan19 <dan35qc@hotmail.com>
2022-01-02 20:44:38 -0500

    main

--- ros-gazebo/docker-build.sh
+++ ros-gazebo/docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build 
+docker build -t magicdan/ros-gazebo .
--- ros-gazebo/dockerhub-publish.sh
+++ ros-gazebo/dockerhub-publish.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -it --rm --privileged --net=host ros_tb2 /bin/bash
+echo "*** commit ***"
+docker container commit dan magicdan/ros-gazebo:latest
+echo "*** push ***"
+docker image push magicdan/ros-gazebo
+echo "*** fin ***"

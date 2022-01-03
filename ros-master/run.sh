3dfe339999f95eb273bea75130fe276382cdebd8
magicdan19 <dan35qc@hotmail.com>
2022-01-02 20:26:17 -0500

    main

--- ros-master/run.sh
+++ ros-master/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -it --ip 192.168.50.222 --rm --name dan ros-master roscore
+# docker network create --subnet=192.168.50.0/24 magicdan
+
+docker run -it -e ROS_MASTER_IP=http://192.168.50.222:11311 -e ROS_IP=192.168.50.222 --net magicdan \
+--ip 192.168.50.222 --rm --name dan ros-master  roscore

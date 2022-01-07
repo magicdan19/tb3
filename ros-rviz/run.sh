0e8f9a31c7f9654e31ddbe2776e2caf6475a71b3
magicdan19 <dan35qc@hotmail.com>
2022-01-07 11:11:19 -0500

    main

--- ros-rviz/run.sh
+++ ros-rviz/run.sh
@@ -4,4 +4,5 @@ docker run -it --rm --privileged --net=host \
 --env=NVIDIA_VISIBLE_DEVICES=all --env=NVIDIA_DRIVER_CAPABILITIES=all \
 --env=DISPLAY --env=QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix \
 --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 \
---name rviz magicdan/ros-rviz /bin/bash
+--name rviz magicdan/ros-rviz \
+/bin/bash
--- ros-teleop/run.sh
+++ ros-teleop/run.sh
@@ -2,4 +2,8 @@
 
 docker run -it --rm --net=host \
 --privileged -v /dev/input/js0:/dev/input/js0 \
---name teleop magicdan/ros-teleop roslaunch teleop_twist_joy teleop.launch
+--name teleop magicdan/ros-teleop \
+roslaunch teleop_twist_joy teleop.launch
+#rosrun joy joy_node
+#/bin/bash
+#roslaunch teleop_twist_joy teleop.launch
